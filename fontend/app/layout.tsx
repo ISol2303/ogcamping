17e3725d2dfaed1ec1470d9bbd1e54efc988803f
ISol2303 <tp.nam23@gmail.com>
2025-09-15 12:43:18 +0700

    Update layout.tsx

--- fontend/app/layout.tsx
+++ fontend/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
   return (
-     <html lang="vi">
+     <html lang="vi" className="mdl-js">
       <body className={`${inter.variable} ${inter.className}`} suppressHydrationWarning>
         <AuthProvider>
           <ChatProvider>
